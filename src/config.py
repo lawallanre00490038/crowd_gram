1442db009a83f4f433bfef5e1d27179be25bf1a8
Ibrahim Abayomi Ogunbiyi <Ibrahimabayomi234@gmail.com>
2025-07-16 14:16:25 +0100

    Created Leaderboard Flow (Command and Sending Automatically to Channel

--- src/config.py
+++ src/config.py
@@ -7,3 +7,4 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 DATABASE_URL = os.getenv("DATABASE_URL")
 ADMIN_IDS = [int(id_) for id_ in os.getenv("ADMIN_IDS", "").split(",") if id_]
 CLOUD_STORAGE = os.getenv("CLOUD_STORAGE", "s3")
+CHANNEL_ID = os.getenv("CHANNEL_ID")
