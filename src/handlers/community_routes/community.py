d9c0f4c429a6561c6812d1be7ee5b0e674401345
tacheampong <th.acheampong@gmail.com>
2025-07-25 11:06:32 -0400

    create task for broadcasting projects, policies, and training and wellness

--- src/handlers/community_routes/community.py
+++ src/handlers/community_routes/community.py
@@ -6,6 +6,8 @@ from .broadcast import (
     get_top_agent_this_week,
     send_leaderboard_weekly,
     send_monthly_contest,
+    send_projects_trainings_policies,
+    send_wellness_weekly,
 )
 
 router = Router()
@@ -14,3 +16,6 @@ async def start_community_background_tasks():
     asyncio.create_task(send_leaderboard_weekly())
     asyncio.create_task(get_top_agent_this_week())
     asyncio.create_task(send_monthly_contest())
+    asyncio.create_task(send_projects_trainings_policies())
+    asyncio.create_task(send_wellness_weekly())
+    
